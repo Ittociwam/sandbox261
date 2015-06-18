dbb7ec20fae4d75b9287779677db1aa523e0081e
Robbie <ittociwam@hotmail.com>
2015-06-18 09:42:40 -0600

    added some app design stuff
    Signed-off-by: Robbie <ittociwam@hotmail.com>

--- Robbie/DOMmanip.js
+++ Robbie/DOMmanip.js
@@ -1,8 +1,103 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
+ * To casdfhange this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  * DOM Manipulation Using createElement, appendChild, insertBefore, removeChild, etc.
  */
 
+function domManip() {
+    //Happy
+    var p = document.createElement("p");
+    var text = document.createTextNode("Hello Dom Manipulation!");
+    p.appendChild(text);
+    document.body.appendChild(p);
 
+// nasty path - throws TypeError parameter 1 is not of type 'Node' can't just send a string 
+// must be a node
+    try {
+        var p = document.createElement("p");
+        p.appendChild("tryign to append a string");
+        document.body.appendChild(p);
+    }
+    catch (e) {
+
+    }
+
+// happy path. I suppose this makes an element called asdf and assigns text to it
+// you must be able to name your elements whatever you want.
+    var asdf = document.createElement("asdf");
+    var text = document.createTextNode("What element is asdf?");
+    asdf.appendChild(text);
+    document.body.appendChild(asdf);
+
+// happy path. This adds text to empty heading tags on my document
+    var title = document.createTextNode("Dom Manipulation!");
+    document.getElementById("myh1").appendChild(title);
+
+// nasty path - this appends a child to head. will not display in the document.
+    var par = document.createElement("p");
+    var text = document.createTextNode("Appending child to head?");
+    par.appendChild(text);
+    document.head.appendChild(par);
+
+// Happy insert before
+    var aDiv = document.createElement("div");
+    var name = document.createTextNode("Insert before heading!");
+    aDiv.appendChild(name);
+    document.body.insertBefore(name, document.getElementById("myh1"));
+
+
+// nasty path. you can't insert a node before a node that 
+// is not a child of the specified node. In this case myh1.
+    try {
+        var aDiv = document.createElement("div");
+        var name = document.createTextNode("Insert before heading inside of heading?");
+        aDiv.appendChild(name);
+        document.getElementById("myh1").insertBefore(name, document.getElementById("myh1"));
+    }
+    catch (e) {
+    }
+
+//happy path - removes the child node (text?) of myh1
+    document.getElementById("myh1").removeChild(document.getElementById("myh1").childNodes[0]);
+
+//    try {
+////nasty path - throwws exception The node to be removed is not a child of this node?
+//        document.removeChild(document.body);
+//    }
+//    catch (e) {
+//
+//    }
+//    try {
+////nasty path - same error as above
+//        document.removeChild(document.body[0]);
+//    }
+//    catch (e) {
+//
+//    }
+//    try {
+////nasty path - same error as above
+//        var body = document.body;
+//        document.removeChild(body);
+//    }
+//    catch (e) {
+//
+//    }
+//    // nasty path - cannot remove children of all? thorws error.
+//var all = document.getElementsByTagName("*");
+//
+//for (var i=0; i < all.length; i++) {
+//     all.removeChild(all.childNodes[i]);
+//     //Throws cannot read property 0 of undefined.
+//}
+
+//happy/nasty path - this takes an existing element and puts it into
+// another element. good or bad depending on what you are trying to do
+
+// var ele = document.getElementById("myh1")
+// document.getElementById("adiv").appendChild(ele);
+
+// nasty path - Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
+document.getElementById("adiv").appendChild(null);
+
+}
--- Robbie/trans.js
+++ Robbie/trans.js
@@ -23,10 +23,11 @@ function alterDiv() {
 
 // Happy path - this is the correct way to alter multiple properties at one and have them
 // happen one at a time
-    ele2.style.transition = "opacity 1s linear 1s, width 1s, background 1s linear 1s";
+    ele2.style.transition = "opacity 1s linear 1s, width 1s, background 1s linear 1s, width 1s";
     ele2.style.opacity = .5;
     ele2.style.width = "400px";
     ele2.style.background = "green";
+    ele2.style.width = "200px";
     
     // nasty path - div will not slowly transition when it is absolute in css
    var ele3 = document.getElementById("div3");
@@ -37,4 +38,16 @@ function alterDiv() {
    var ele3 = document.getElementById("div3");
    ele3.style.transition = "left 2s linear 1s";
    ele3.style.left = "194px";
+
+// happy path - setting the transition in the css
+   var ele4 = document.getElementById("div4");
+   ele4.style.height = "400px";
+   ele4.style.width = "300px";
+   ele4.style.background = "blue";
+
+   // happy path - setting the transition in the css
+   var ele5 = document.getElementById("div5");
+
+     ele5.style.transform = "rotate(90deg)";
+
 }
